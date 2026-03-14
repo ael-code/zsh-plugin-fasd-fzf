ef6300777d9d823596b9638398202670f82ab6f7
ael-code <tommy.ael@gmail.com>
2026-03-14 08:45:57 +0100

    Adapt fzf options for fzf versions >= 0.53.0
    Starting with fzf 0.53.0 fzf has native tmux integration through the
    `--tmux` CLI option. Thus we can change `fzf-tmux` with `fzf --tmux`.

--- fasd-fzf.plugin.zsh
+++ fasd-fzf.plugin.zsh
@@ -1,7 +1,7 @@
 if [[ $commands[fzf] && $commands[fasd] ]]; then
 	cd-fzf-widget() {
 		local dir
-		dir="$(fasd -Rdl "$1" | fzf-tmux -1 -0 --no-sort --height=16 --reverse --toggle-sort=ctrl-r)" && cd "${dir}"
+		dir="$(fasd -Rdl "$1" | fzf --tmux -1 -0 --no-sort --height=16 --reverse --toggle-sort=ctrl-r)" && cd "${dir}"
 		local ret=$?
 		zle reset-prompt
   		typeset -f zle-line-init >/dev/null && zle zle-line-init
